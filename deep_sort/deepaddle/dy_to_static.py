6d388f7b91662d1bf9cbae4376538e88a8ebadd6
lu1kaifeng <8302078+lu1kaifeng@user.noreply.gitee.com>
2021-03-28 22:39:38 +0800

    new dy_to_static.py

--- deep_sort/deepaddle/dy_to_static.py
+++ deep_sort/deepaddle/dy_to_static.py
@@ -1,3 +1,4 @@
+import numpy as np
 import paddle as torch
 from paddle.fluid.reader import DataLoader
 
@@ -6,7 +7,7 @@ from deep_sort.deepaddle.model import Net
 
 model_path = 'checkpoint/net'
 model = torch.jit.load(model_path)
-net = Net()
+net = Net(reid=True)
 net.set_state_dict(model.state_dict())
 
 from paddle.jit import TracedLayer
@@ -16,17 +17,21 @@ import os
 root = 'data/archive/Market-1501-v15.09.15'
 train_dir = os.path.join(root, "bounding_box_train")
 test_dir = os.path.join(root, "bounding_box_test")
-transform_train = torchvision.transforms.Compose([
-    torchvision.transforms.RandomCrop((128, 64), padding=4),
-    torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+size = (64, 128)
+norm = torchvision.transforms.Compose([
+    # torchvision.transforms.ToTensor(data_format='CHW'),
+    torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225], data_format='HWC'),
 ])
 trainloader = DataLoader(
-    ReIDDataset(train_dir, transform=transform_train),
+    ReIDDataset(train_dir, transform=norm),
     batch_size=1, shuffle=True
 )
 for ip in trainloader:
-    out_dygraph, static_layer = TracedLayer.trace(net, inputs=ip[0])
-    static_layer.save_inference_model('./checkpoint_static/net', feed=[0], fetch=[0])
+    #out_dygraph, static_layer = TracedLayer.trace(net, inputs=np.moveaxis(ip[0], (3), (1)))
+    input_spec = torch.static.InputSpec(shape=[1, 3, 128, 64], dtype='float32')
+    net = torch.jit.to_static(net, input_spec=[input_spec])
+    net(np.moveaxis(ip[0], (3), (1)))
+    torch.jit.save(model, './checkpoint_static/net',input_spec=[input_spec])
+    #print(static_layer(inputs=np.moveaxis(ip[0], (3), (1))))
+    #static_layer.save_inference_model('./checkpoint_static/net', feed=[0], fetch=[0])
     break
