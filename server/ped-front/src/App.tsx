4730bc761471e38b92d6d2877efd0cc1fbdaae22
lu1kaifeng <8302078+lu1kaifeng@user.noreply.gitee.com>
2021-04-18 14:32:07 +0800

    video display

--- server/ped-front/src/App.tsx
+++ server/ped-front/src/App.tsx
@@ -1,28 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { io } from 'socket.io-client'
+import axios from 'axios'
 import { Navbar } from './components/Navbar'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 function useFrame() {
-  // const [isOnline, setIsOnline] = useState(null)
+  const [frame, setFrame] = useState<Blob | null>(null)
   useEffect(() => {
     const socket = io()
-    socket.on('frame', (args) => {
-      console.log(args)
+    socket.on('frame', (args: ArrayBuffer) => {
+      setFrame(new Blob([args], { type: 'image/jpeg' }))
     })
-  })
+    axios.get('/api')
+  }, [])
+  return frame
+}
+
+function useTrack() {
+  const [track, setTrack] = useState<any | null>(null)
+  useEffect(() => {
+    const socket = io()
+    socket.on('track', (args) => {
+      setTrack(args)
+    })
+    axios.get('/api')
+  }, [])
+  return track
 }
 
 const App: React.FC = () => {
-  useFrame()
+  const frame = useFrame()
+  useTrack()
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container">
         <Switch>
+          {frame !== null && <img width={1024} src={window.URL.createObjectURL(frame)} alt='penis'/>}
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
         </Switch>
--- server/ped-front/src/setupProxy.js
+++ server/ped-front/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 var cors = require('cors')
 module.exports = function(app) {
   app.use(
-    ['/socket.io','/sockjs-node'],
+    ['/socket.io','/sockjs-node','/api'],
     createProxyMiddleware({
       target: 'http://localhost:5000',
       changeOrigin: true,
